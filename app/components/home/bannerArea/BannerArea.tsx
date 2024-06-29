dc734b82e0ba4abd9ee70bf678a3bb7dbab279e1
thanghn <thanghongoc@tcom.vn>
2024-06-29 15:59:45 +0700

    update fontawesome

--- app/components/home/bannerArea/BannerArea.tsx
+++ app/components/home/bannerArea/BannerArea.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function BannerArea() {
-  return <div>BannerArea</div>;
+  return (
+    <div>
+      <p>BannerArea</p>
+      <div className="w-6 h-6"><FontAwesomeIcon icon={faEnvelope} /></div>
+    </div>
+  );
 }
 
 export default BannerArea;
